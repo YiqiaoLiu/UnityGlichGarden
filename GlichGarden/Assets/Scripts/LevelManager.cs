a8614b0b13c57e43a89c38fdde2d1319930abe73
yl354@duke.edu <yl354@duke.edu>
2016-04-12 12:07:12 -0400

    Add a splash image and a needed to changing Start menu

---
+++ GlichGarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevel;
+	void Start() {
+		Invoke ("LoadNextLevel", autoLoadNextLevel);
+	}
+
+	public void LoadLevel(string name) {
+		Debug.Log ("A level load requested to: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void LoadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log("I want to quit the game!");
+		Application.Quit();
+	}
+}
