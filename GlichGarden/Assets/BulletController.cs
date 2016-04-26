3c6e4d84975ed075b824d61e1478ffb435f4a3bd
yl354@duke.edu <yl354@duke.edu>
2016-04-26 01:42:12 -0400

    Bullet tidy and add script to control it
    add cs

---
+++ GlichGarden/Assets/BulletController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	private float bulletSpeed = 5f;
+	public float bulletDamage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * bulletSpeed * Time.deltaTime);
+	}
+}
