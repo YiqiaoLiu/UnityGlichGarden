adb9088f12566bfb1e171e0ccee52aa973e8ec61
yl354@duke.edu <yl354@duke.edu>
2016-04-27 00:57:00 -0400

    Add the firecontrol function

---
+++ GlichGarden/Assets/RightBound.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightBound : MonoBehaviour {
+
+	//If some object out of this bound destroy it!
+	void OnTriggerEnter2D(Collider2D col){
+		Destroy (col.gameObject);
+	}
+}
--- GlichGarden/Assets/Scripts/Defender/DefenderController.cs
+++ GlichGarden/Assets/Scripts/Defender/DefenderController.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class DefenderController : MonoBehaviour {
 	
+	private Shooter shoot;						//Define the shooter variable
+	private Animator anim;						//Define the Animator variable
+
 
 	// Use this for initialization
 	void Start () {
-	
+		shoot = GetComponent<Shooter> ();		//Get the shooter variable
+		anim = GetComponent<Animator> ();		//Get the Animator variable
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 	
 }
