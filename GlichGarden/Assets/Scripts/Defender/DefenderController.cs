6a336e9c153d547146f0d75ff7d43ecf7570af1e
yl354@duke.edu <yl354@duke.edu>
2016-04-30 10:41:04 -0400

    Add a simple star controller

--- GlichGarden/Assets/Scripts/Defender/DefenderController.cs
+++ GlichGarden/Assets/Scripts/Defender/DefenderController.cs
@@ -5,17 +5,23 @@ public class DefenderController : MonoBehaviour {
 	
 	private Shooter shoot;						//Define the shooter variable
 	private Animator anim;						//Define the Animator variable
+	private ShowStarNumber star;
 
 
 	// Use this for initialization
 	void Start () {
 		shoot = GetComponent<Shooter> ();		//Get the shooter variable
 		anim = GetComponent<Animator> ();		//Get the Animator variable
+		star = GameObject.FindObjectOfType<ShowStarNumber> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	public void addStar(int vol){
+		star.addStarNumber (vol);
 	}
 	
 }
